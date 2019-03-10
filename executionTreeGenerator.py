0bb896a9f5ef158f85073f063777f5f5914a5f02
MTamara95 <tamaramarcetic@hotmail.com>
2019-03-10 22:04:37 +0100

    Added source code line numbers in conditional nodes

--- executionTreeGenerator.py
+++ executionTreeGenerator.py
@@ -2,9 +2,37 @@ import os
 import sys
 import treeKlee as tk
 
+# formiramo listu linija od interesa - koje se ticu linija source code-a u kojima su uslovi
+# - to su linije oblika X lineNum X ... X Q X X X \n
+def lineNumbersOfSourceCode():
+  lines = []
+  f = open("./klee-last/run.istats", "r")
+  ind = 0
+  while(True):
+    currLine = f.readline();
+    if(currLine[0:2] == "fn"): # currLine = "fn=..." - starting point of file run.istats
+      break
+  while(True):
+    currLine = f.readline().split(' ')
+    if(currLine[0][0:2] == "fn"): # when currLine = "fn=main", we need to stop
+      break
+    # Q = currLine[len(currLine)-5]
+    if(len(currLine) > 4 and ind == 0 and currLine[len(currLine)-5] != '0'): # prvi uslov samo jednom
+      lines.append(int(currLine[1]))
+      ind = 1
+    elif(len(currLine) > 4 and ind == 1 and currLine[len(currLine)-5] != '0'): # uslovi koji se ticu nezavisnih if-ova treba da se nadovezuju svuda, ovde ih uzimamo odgovarajuci broj puta (ali jos uvek nisu na adekvatnim indeksima...)
+      for j in range (0, int(currLine[len(currLine)-5])):
+        lines.append(int(currLine[1]))
+  return lines
+
 treeData = tk.getPaths("klee-last/symPaths.ts")
 treeDataItems = treeData.items()
-treeDataItems.sort()
 treeBranches = tk.getTree(treeDataItems)
-tk.drawTree(treeBranches, len(treeDataItems))
-
+preorder = treeBranches.PreorderTraversal(treeBranches) # KLD obilazak cvorova (uslovi, ali i listovi)
+preorder = tk.formConditionNodes(treeBranches, preorder) # -> KLD obilazak cvorova u kojima su uslovi
+mapPreorder = {} # mapiramo jer zelimo da budu uparene sa odgovarajucim linijama iz fajla "run.istats"
+lines = lineNumbersOfSourceCode()
+for i in range (0, len(preorder)):
+# moramo nekako obraditi kada postoji vise nezavisnih uslova... - tu su sve linije odgovarajuci broj puta, ali ne u odgovarajucem redosledu
+  mapPreorder[preorder[i]] = lines[i]
+tk.drawTree(treeBranches, len(treeDataItems), mapPreorder)
--- treeKlee.py
+++ treeKlee.py
@@ -6,12 +6,10 @@ from binarytree import Node
 import matplotlib.pyplot as plt
 
 # funkcija koja cita mapu iz symPaths.ts fajla:
-
 def getPaths(path):
     data = open(path,'rb').read() # Otvaramo binarni fajl za citanje
     paths = { 0 : ''} # Mapa u kojoj cemo cuvati putanje
     pos = 0 # Indikator dokle smo stigli sa citanjem
-    
     # prolazak kroz fajl i popunjavanje mape
     while pos<len(data):
         id,tag = struct.unpack('II', data[pos:pos+8])
@@ -25,7 +23,6 @@ def getPaths(path):
             pos += size
     if pos!=len(data):
         raise IOError
-    
     return paths
 
 # funkcija koja cita strukturu stabla iz liste:
@@ -46,34 +43,35 @@ def getPaths(path):
 
 def getTree(treeList):
     root = Node(0)
-    
+    k = 1
+    # ovde namestiti da oni sa istim uslovom da imaju isto k...?
     for i in range(1, len(treeList)):
         branch = (treeList[i])[1]
         tmp = root
         for j in range(0, len(branch)):
             if(branch[j] == '1'):
                 if(not tmp.left):
-                    tmp.left = Node(i)
+                    tmp.left = Node(k) #i
+		    k = k+1
                 tmp = tmp.left
             elif(branch[j] == '0'):
                 if(not tmp.right):
-                    tmp.right = Node(i)
+                    tmp.right = Node(k) #i
+		    k = k+1
                 tmp = tmp.right
-    
     return root
-    
 
 # funkcija dobija strukturu stabla i iscrtava ga
-def drawTree(tree, leaves):
+def drawTree(tree, leaves, mapPreorder):
     
     def text(x, y, t, size=12, **kwargs):
         plt.text(x, y, t, ha='center', va='center', size=size, bbox=dict(boxstyle='round', ec='k', fc='w'), **kwargs)
-    
+
     def drawLines(tree, x, y, dx, dy):
+        preorder = tree.PreorderTraversal(tree)
         ind = False
         dx = dx * 0.5
         dy = dy * 1.2
-       
         if(tree.left):
             ind = True
             text(x-dx/2, y-dy/2, "True", alpha=0.4)
@@ -85,17 +83,41 @@ def drawTree(tree, leaves):
             plt.plot([x, x+dx], [y, y-dy],'-k')
             drawLines(tree.right, x+dx, y-dy, dx, dy)
         if(ind):
-            text(x, y, "uslov", 20)
+            text(x, y, mapPreorder[tree.data], 20) #"uslov"
         else:
             text(x, y, "list", 15)
-            
- 
+
     x = 0.5
     y = 1
     dx = 0.05*(2**(leaves-1))
     dy = 0.1
-            
     drawLines(tree, x, y, dx, dy)    
     plt.show()
 
+# funkcija za KLD obilazak stabla:
+class Node:
+    def __init__(self, data):
+        self.left = None
+        self.right = None
+        self.data = data
 
+    def PreorderTraversal(self, root):
+        res = []
+        if root:
+            res.append(root.data)
+            res = res + self.PreorderTraversal(root.left)
+            res = res + self.PreorderTraversal(root.right)
+        return res
+
+# preorder sadrzi i uslove i listove, sada formiramo listu koja sadrzi KLD obilazak samo uslova:
+def formConditionNodes(tree, preorder):
+  ind = False
+  if(tree.left):
+    ind = True
+    formConditionNodes(tree.left, preorder)
+  if(tree.right):
+    ind = True
+    formConditionNodes(tree.right, preorder)
+  if(ind == False):
+    preorder.remove(tree.data)
+  return preorder

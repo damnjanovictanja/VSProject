59f2f07f201fd11f39568da9cf7823487c7a13ba
MTamara95 <tamaramarcetic@hotmail.com>
2019-03-10 22:09:48 +0100

    Added a few test cases

--- test/even_sym.c
+++ test/even_sym.c
@@ -4,22 +4,65 @@
 
 /* funkcija vraca 1 ukoliko je x paran, inace 0 */
 int even(int x) {
-  if (x%3==0){
-    if(x>5)
-			x++;
-    else {
-			if ()
-		}
-			return 4;
-	}
-  if(x%2){
-    if(x>7)
-			return 11;
-    else
-			x++;
-	}
+// 1. primer
+/*
+  if(x%5 == 0)
+    x--;
+  else
+    x++;
+  if(x%4 == 0)
+    x+=2;
+  else
+    x-=2;
+  if(x%3 == 1)
+    x++;
+  else
+    x--;
+*/
+// 2. primer
+/*
+  if(x%5 == 0){
+    x *= -2;
+  }
+  if(x < 5){
+    if(x < -1){
+      x = 1;
+    }
+    x = 2;
+  }
+*/
+// 3. primer
 
-  klee_assert(0); 
+  if(x > 5){
+    if(x >= 10){
+      if(x%2 == 0){
+	x = 2;
+      }
+      else{
+	x = 3;
+      }
+    }
+    else{
+      if(x % 2 != 0){
+        x++;
+      }
+      else{
+	x /= 2;
+      }
+    }
+  }
+  else{
+    //x = 11;
+    if(x < 3){
+      x *= 12;
+    }
+    else{
+      x--;
+    }
+  }
+
+
+  klee_assert(0);
   return 1; // nedostizna naredba
 }
 

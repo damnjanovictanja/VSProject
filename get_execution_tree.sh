13c5a968001fb3114f06e03f8adc1fec30833233
Tanja Damnjanović <damnjanovic.tanja96@gmail.com>
2019-04-04 11:20:12 +0200

    Add .c file formatting with clang-format

--- get_execution_tree.sh
+++ get_execution_tree.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 cd test
+/home/student/build/llvm/Release/bin/clang-format -i $1.c
 /home/student/build/llvm/Release/bin/clang -I ~/build/klee/include -emit-llvm -c -g $1.c
 /home/student/build/klee/Release+Asserts/bin/klee --write-sym-paths $1.bc
 cd ..
